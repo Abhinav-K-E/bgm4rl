c51465f8445eb8bcf44adddd533b6de07d693130
Abhinav K E <109943270+Abhinav-K-E@users.noreply.github.com>
2024-11-01 22:25:01 +0530

    chat animation added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,21 +1,28 @@
-import React, { useState } from 'react'
-import PreLoader from '../../components/PreLoader'
-import Nav from '../../components/Nav/Nav';
+import React, { useState } from "react";
+import PreLoader from "../../components/PreLoader";
+import Nav from "../../components/Nav/Nav";
 
 const Home = () => {
-    const [preloader,setPreLoader]=useState(true);
-    setTimeout(()=>{
-        setPreLoader(false)
-    },3000)
-    console.log(preloader)
+  const [preloader, setPreLoader] = useState(true);
+  setTimeout(() => {
+    setPreLoader(false);
+  }, 3000);
+  console.log(preloader);
   return (
-    <div className='home'>
-        {
-            preloader && <PreLoader/>
-        }
-        <Nav/>
+    <div className="home">
+      {preloader && <PreLoader />}
+      <Nav />
+      <div className="chat-ui">
+        <div>
+          <div className="scale-0">
+            <div className="circle circle-1" />
+            <div className="circle circle-2" />
+            <div className="circle circle-3" />
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
